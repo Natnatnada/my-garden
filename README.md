5eeafb9f8a6419ad760b312988b96402bf56eeb1
Natalia Aspee <n.aspeedelgado@gmail.com>
2023-05-16 17:18:13 -0400

    :tada: Layout html

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# my-garden
+# Mi Jardín
+
+## Plants from my garden
+
+### The project is a web page that allows users to visualize and filter the different types of plants in a garden
+### It was developed using Js Vanilla, CSS, and HTML, with a static database used to store the plant information provided by the client.
---
+++ index.js
@@ -0,0 +1,43 @@
+//db clients plant
+const plants = [
+    {
+        id: 1,
+        name: 'Monstera',
+        type: 'interior',
+        img: './images/monstera.jpg',
+        description: 'Planta de interior, no soporta sol directo'
+
+    },
+    {
+        id: 2,
+        name: 'Monstera Adansonii',
+        type: 'interior',
+        img: './images/adansonii.jpg',
+        description: 'Tambien conocida como cheese plant por los orificios de sus hojas'
+
+    },
+    {
+        id: 3,
+        name: 'Aeonium Arboreum',
+        type: 'suculenta',
+        img: './images/rosanegra.jpg',
+        description: 'Tambien conocida como rosa negra, es de sol directo'
+
+    },
+    {
+        id: 4,
+        name: 'Cotyledon Tomentosa',
+        type: 'suculenta',
+        img: '',
+        description: 'Tambien conocida como oreja de oso'
+
+    }
+
+
+];
+
+const centerSection = document.querySelector('.center');
+console.log(centerSection)
+window.addEventListener('DOMContentLoaded', function(){
+    console.log('hello')
+})
